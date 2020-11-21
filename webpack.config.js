677932ffbf898e2818d38d22d02a0b64f7fa3c6f
squaredbusinessman <burdaonelove@gmail.com>
2020-11-21 18:23:59 +0300

    поправил опечатку в requirements, сделал точку входа с использованием path.resolve

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: path.resolve('src', 'main.js'),
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, 'public'),
