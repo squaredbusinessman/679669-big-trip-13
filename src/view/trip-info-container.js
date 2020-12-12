188c35fa3a0002940815fd1164d9205679686ea1
squaredbusinessman <burdaonelove@gmail.com>
2020-12-12 17:43:41 +0300

    работа с моками, попытки подставить их в шаблон

---
+++ src/view/trip-info-container.js
@@ -0,0 +1,3 @@
+export const createTripInfoContainerTemplate = () => {
+  return `<section class="trip-main__trip-info  trip-info"></section>`;
+};
---
+++ src/view/waypoints-container.js
@@ -0,0 +1,3 @@
+export const createWaypointsContainer = () => {
+  return `<ul class="trip-events__list"></ul>`;
+};
