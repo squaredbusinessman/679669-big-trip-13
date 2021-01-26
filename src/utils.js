533924e58cbb3ac8a7733645a8dbefcd241ff4cb
squaredbusinessman <burdaonelove@gmail.com>
2021-01-26 21:37:10 +0300

    Добавил абстрактный класс

--- src/utils.js
+++ src/utils.js
@@ -44,3 +44,11 @@ export const renderTemplate = (container, template) => {
 export const sortTripEvents = (events) => {
   return events.slice().sort((a, b) => a.start - b.start);
 };
+
+// функция фиксации необходимого нам контекста
+
+export const bindContext = function (context, fn) {
+  return function (...args) {
+    return fn.call(context, ...args);
+  };
+};
